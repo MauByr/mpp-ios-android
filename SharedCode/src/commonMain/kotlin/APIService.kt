8c369791db56de5dad5bec986830269b6f6bad51
Maurice Byrne <Maurice.Byrne@softwire.com>
2022-06-29 15:46:23 +0100

    fixed dateTime

--- SharedCode/src/commonMain/kotlin/APIService.kt
+++ SharedCode/src/commonMain/kotlin/APIService.kt
@@ -2,8 +2,7 @@ package com.jetbrains.handson.mpp.mobile
 
 import com.jetbrains.handson.mpp.mobile.dataObjects.FareResponse
 import com.jetbrains.handson.mpp.mobile.dataObjects.StationList
-import com.soywiz.klock.DateTime
-import com.soywiz.klock.DateTimeTz
+import com.soywiz.klock.*
 import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
@@ -16,8 +15,8 @@ object APIService {
         const val STATIONS = "stations/"
     }
 
-    private const val rootURL = "https://mobile-api-softwire2.lner.co.uk/v1/"
-    fun httpClient():HttpClient = HttpClient { install(JsonFeature) }
+    const val rootURL = "https://mobile-api-softwire2.lner.co.uk/v1/"
+    private fun httpClient():HttpClient = HttpClient { install(JsonFeature) }
 
     suspend fun getStationList(): StationList? {
         return  makeWebRequest(
@@ -38,20 +37,29 @@ object APIService {
             default
         }
     }
-
-
 }
 
-class JourneyQuery(private val from: String, private val to: String, private val time: DateTimeTz = DateTime.nowLocal()) {
+class JourneyQuery(private val from: String, private val to: String, private val time: DateTimeTz = DateTime.nowLocal()
+) {
+
     fun getUrl(): Url {
-        val builder = URLBuilder(rootURL+"fares/")
+        val builder = URLBuilder(APIService.rootURL +"fares")
         with(builder) {
             parameters["originStation"] = from
             parameters["destinationStation"] = to
-            parameters["outboundDateTime"] = time.format("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+            parameters["outboundDateTime"] = calcSearchTime().format("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
             parameters["numberOfAdults"] = "1"
             parameters["numberOfChildren"] = "0"
         }
         return builder.build()
     }
+    private fun calcSearchTime():DateTimeTz{
+        val timeOffset = TimeSpan(60000.0)
+        val timeDiff = time - DateTime.nowLocal()
+        if(timeDiff<timeOffset){
+            return time.addOffset(timeOffset-timeDiff)
+        }
+        return time
+    }
 }
+
