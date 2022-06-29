de2b6260f431ddf85569a3203447f2ce414a0949
Gabriel Swallow <gabriel.swallow@softwire.com>
2022-06-29 10:34:38 +0100

    button responding to API Fare  GET

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -6,9 +6,11 @@ class ViewController: UIViewController {
     @IBOutlet var fareSearchButton: UIButton!
     @IBOutlet var trainsTable: UITableView!
 
-    //@IBAction func doSomething(){onSearchClicked()}
-    
-    let validTrains = ["1", "2", "3", "4"]
+    @IBAction func buttonPressed(_ sender: Any) {
+        presenter.onSearchClicked()
+    }
+
+    var validTrains = ["1", "2", "3", "4"]
 
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
     
@@ -26,6 +28,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         self.registerTableViewCells()
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.validTrains.count
     }
@@ -41,15 +44,28 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
     private func registerTableViewCells() {let textFieldCell = UINib(nibName: "customTableViewCell", bundle: nil)
         self.trainsTable.register(textFieldCell, forCellReuseIdentifier: "customTableViewCell")
     }
+    
+    private func fillTable(){
+        
+    }
 }
 
 
 extension ViewController: ApplicationContractView {
     
     func showResults(result: FareResponse) {
-        // TODO: not yet implemented
+        print("DATA")
+        print(result.outboundJourneys.count)
+        validTrains.removeAll()
+        validTrains = result.outboundJourneys.map(displayJourney(journey:))
+        print(validTrains)
+        trainsTable.reloadData()
     }
-
+    
+    private func displayJourney(journey :Journey) -> String{
+        return "_"
+    }
+    
     func populateStationList(stations: [String]) {
         // TODO: not yet implemented
     }
