a7798366d1f9582c8c176af7463dba72e19ea362
Gabriel Swallow <gabriel.swallow@softwire.com>
2022-07-04 17:29:50 +0100

    began storyboard for search page on IOS

---
+++ native/KotlinIOS/KotlinIOS/searchPage.swift
@@ -0,0 +1,14 @@
+//
+//  searchPage.swift
+//  KotlinIOS
+//
+//  Created by Gabriel Swallow on 04/07/2022.
+//  Copyright © 2022 Evgeny Petrenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchPageViewController: UIViewController {
+    
+}
