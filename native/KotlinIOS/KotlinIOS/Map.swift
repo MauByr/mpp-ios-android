0afb446f1258195beb6ae48cb07eac5458100712
Gabriel Swallow <gabriel.swallow@softwire.com>
2022-07-08 14:26:49 +0100

    addPinToMap() added

--- native/KotlinIOS/KotlinIOS/Map.swift
+++ native/KotlinIOS/KotlinIOS/Map.swift
@@ -21,18 +21,29 @@ class Map: UIViewController{
     weak var delegate: MapDelegate?
     
     override func viewDidLoad(){
-        //mapView.region
+        super.viewDidLoad()
+        
+        addPinToMap(latitude: 51, longitude: 0)
     }
     
-    public static func createMapPage() -> Map {
-        let newInstance = Map()
-        return newInstance
+    
+    private func addPinToMap(latitude: Double, longitude: Double){
+        let annotation = MKPointAnnotation()
+        let centerCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        annotation.coordinate = centerCoordinate
+        annotation.title = "station name here"
+        mapView.addAnnotation(annotation)
     }
+//    public static func createMapPage() -> Map {
+//        let newInstance = Map()
+//        return newInstance
+//    }
     
 //    private func closeMapPage(){
 //        dismiss(animated: true)
 //    }
 }
+
 
 protocol MapDelegate: AnyObject{
     func getMapData() -> AnyObject
