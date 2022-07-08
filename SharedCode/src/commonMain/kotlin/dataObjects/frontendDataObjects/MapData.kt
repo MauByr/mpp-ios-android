98a0e4374866ff4f27ccba513070672c365402f5
Maurice Byrne <Maurice.Byrne@softwire.com>
2022-07-08 14:12:20 +0100

    Added fields to MapData.kt

--- SharedCode/src/commonMain/kotlin/dataObjects/frontendDataObjects/MapData.kt
+++ SharedCode/src/commonMain/kotlin/dataObjects/frontendDataObjects/MapData.kt
@@ -1,3 +1,16 @@
 package com.jetbrains.handson.mpp.mobile.dataObjects.frontendDataObjects
 
-class MapData
+data class MapData(
+    val startStation: JourneyWayPoint,
+    val endStation: JourneyWayPoint
+)
+
+data class JourneyWayPoint(
+    val name: String,
+    val coords: GeoCoordinate
+)
+
+data class GeoCoordinate(
+    val latitude: Double,
+    val longitude: Double
+)
